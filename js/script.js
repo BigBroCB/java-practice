197aaea2774bd8a3c496af8efa06e6ffe641c0b7
BigBroCB <christopherbanks716@gmail.com>
2022-11-07 11:05:28 -0500

    added comments and first logs

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+const appId = document.getElementById("app-Id");
+let myApp = document.getElementById("my.app");
+
+appId.innerText="Hi world";
+myApp.innerText="Hello world";
+
+appId.innerHTML="<h2>Hello students</h2>";
+
+//Happy Birthday//
+
+/* my birthay
+is in  a few moths*/
+console.log('My Name Is Christopher Banks');
+console.log('My Fav Food Is Chicken')
